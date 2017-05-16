3da0f0848d8be341142be25b05a6f2340eaa07eb
votafore <a.zakharov.druzh@gmail.com>
2017-05-16 10:56:27 +0300

    начало разработки страницы списков

--- app/src/main/java/com/votafore/songbook/ActivityMain.java
+++ app/src/main/java/com/votafore/songbook/ActivityMain.java
@@ -1,9 +1,12 @@
 package com.votafore.songbook;
 
+import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityMain extends AppCompatActivity {
 
@@ -14,7 +17,23 @@ public class ActivityMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        Button find = (Button) findViewById(R.id.find_songs);
+        Button view = (Button) findViewById(R.id.view_songs);
+
+        find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityMain.this, ActivityList.class);
+                startActivity(intent);
+            }
+        });
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
