357241738e0cdc9138f863eb86dbb9fdbf470688
votafore <a.zakharov.druzh@gmail.com>
2017-05-16 16:02:56 +0300

    v 1.0.1

--- app/src/main/java/com/votafore/songbook/ActivityList.java
+++ app/src/main/java/com/votafore/songbook/ActivityList.java
@@ -4,9 +4,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.votafore.songbook.support.FragmentPage12;
-import com.votafore.songbook.support.FragmentPage34;
-import com.votafore.songbook.support.FragmentPage56;
+import com.votafore.songbook.support.FragmentPage;
 import com.votafore.songbook.support.PagerAdapter;
 
 public class ActivityList extends AppCompatActivity {
@@ -20,9 +18,9 @@ public class ActivityList extends AppCompatActivity {
 
         PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
 
-        adapter.addPage(new FragmentPage12());
-        adapter.addPage(new FragmentPage34());
-        adapter.addPage(new FragmentPage56());
+        adapter.addPage(FragmentPage.getInstance("1 - 2"));
+        adapter.addPage(FragmentPage.getInstance("3 - 4"));
+        adapter.addPage(FragmentPage.getInstance("5 - 6"));
 
         pager.setAdapter(adapter);
     }
