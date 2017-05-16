b19728d5af5c6694e95000307fc6565e50bb139b
votafore <a.zakharov.druzh@gmail.com>
2017-05-16 09:35:56 +0300

    start 2

---
+++ app/src/main/java/com/votafore/songbook/ActivityMain.java
@@ -0,0 +1,20 @@
+package com.votafore.songbook;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class ActivityMain extends AppCompatActivity {
+
+    private ViewPager pager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
