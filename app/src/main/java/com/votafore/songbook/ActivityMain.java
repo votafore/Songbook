198ef0f2c974c04b5e2e26e9d7cd8c21629c6f3b
votafore <a.zakharov.druzh@gmail.com>
2017-06-12 15:14:50 +0300

    убрал лишнее

--- app/src/main/java/com/votafore/songbook/ActivityMain.java
+++ app/src/main/java/com/votafore/songbook/ActivityMain.java
@@ -1,18 +1,12 @@
 package com.votafore.songbook;
 
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.votafore.songbook.fragments.FragmentList;
 
 
 public class ActivityMain extends AppCompatActivity {
 
-
-    FragmentList pageList;
     FragmentManager mFragmentManager;
 
     @Override
@@ -20,15 +14,15 @@ public class ActivityMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_main_activity_ex);
 
-        pageList = FragmentList.newInstance();
-
-        mFragmentManager = getSupportFragmentManager();
-
-        Fragment currentFragment = mFragmentManager.findFragmentById(R.id.container_main);
-
-        if(currentFragment == null)
-            mFragmentManager.beginTransaction()
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                    .add(R.id.container_main, pageList).commit();
+//        pageList = FragmentList.newInstance();
+//
+//        mFragmentManager = getSupportFragmentManager();
+//
+//        Fragment currentFragment = mFragmentManager.findFragmentById(R.id.container_main);
+//
+//        if(currentFragment == null)
+//            mFragmentManager.beginTransaction()
+//                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+//                    .add(R.id.container_main, pageList).commit();
     }
 }
